29efc59a9a0182afaa35d2bf012cce9057351759
Hamza Mehmood <hamza.mehmood@zmwl.local>
2026-01-07 13:11:01 +0100

    Added frontend secure url

--- config/cors.php
+++ config/cors.php
@@ -5,7 +5,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8081'],
+    'allowed_origins' => ['http://localhost:5173', 'http://localhost:8081', 'http://10.0.0.120:81'],
 
     'allowed_origins_patterns' => [],
 
